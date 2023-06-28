8bde917fa4903b0068cf3f28a4dbd76392a1b7c4
ldinkaofficial <klassnyjartem8@gmail.com>
2023-06-29 04:04:21 +0600

    Initial commit

---
+++ src/main/java/ru/apps/engproj/EngProjApplication.java
@@ -0,0 +1,13 @@
+package ru.apps.engproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/apps/engproj/EngProjApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.apps.engproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
